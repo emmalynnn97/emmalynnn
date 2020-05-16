bc10159e9037b11a11f52edd6afbcd8e607b4f45
emmalynnn97 <emmalynnn97@gmail.com>
2020-05-15 23:05:55 -0700

    cleaning stuff up

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 export class Footer extends Component {
     constructor(props) {
         super(props)
@@ -16,12 +17,22 @@ export class Footer extends Component {
             left:'0',
             right:'0',
             width:'100%',
-            height:'10%',
-            backgroundColor:this.state.color
+            height:'12.5%',
+            backgroundColor:this.state.color,
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center'
+        }
+        const iconStyle={
+            height:'50px',
+            width:'50px',
+            color:'white',
+            margin:'0 1vw'
         }
         return (
             <div style={footerStyle}>
-                
+                <a rel='noopener noreferrer' target='_blank' href='https://github.com/emmalynnn97'><FaGithub style={iconStyle}></FaGithub></a>
+                <a rel='noopener noreferrer' target='_blank' href='https://www.linkedin.com/in/emma-guy-639014189/'><FaLinkedin style={iconStyle}></FaLinkedin></a>
             </div>
         )
     }
--- src/Project.js
+++ src/Project.js
@@ -4,40 +4,28 @@ import ProjectOverlay from './ProjectOverlay'
 export class Project extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-            image:this.props.image,
-            title:this.props.title,
-            content:this.props.content,
-            display:'block',
-            width:'30vw',
-            height:'30vh'
+            image: this.props.image,
+            title: this.props.title,
+            content: this.props.content,
+            width: '30vw',
+            height: '30vh'
         }
     }
     render() {
-        const projectStyle={
-            backgroundImage:`url("${this.state.image}")`,
-            backgroundSize:'cover',
-            backgroundPosition:'center',
-            height:this.state.height,
-            width:this.state.width,
-            overflow:'hidden'
-        }
-        const modalStyle={
-            display:this.state.display
+        const projectStyle = {
+            backgroundImage: `url("${this.state.image}")`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: this.state.height,
+            width: this.state.width,
+            overflow: 'hidden'
         }
         return (
-        <>
-      
-        <div className='project col col-lg-4 col-md-6 col-sm-12' style={projectStyle}>
-            <ProjectOverlay content={this.state.content} title={this.state.title}/>
+            <div className='project col col-lg-4 col-md-6 col-sm-12' style={projectStyle}>
+                <ProjectOverlay content={this.state.content} title={this.state.title} />
             </div>
-
-           
-
-          </> 
-                  
-                   
         )
     }
 }
